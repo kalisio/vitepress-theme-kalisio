fd9364490fa59f4bfe3f6be415ef9a3a0888347f
cnouguier <ch.nouguier@gmail.com>
2024-02-02 12:02:13 +0100

    wip: HomeFooter seems to be not defined in production mode #1

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ import _ from 'lodash'
 import DefaultTheme from 'vitepress/theme'
 import { Quasar, Dialog } from 'quasar'
 import KeycloakLayout from './layouts/KeycloakLayout.vue'
-import KHomeFooter from './components/KHomeFooter'
-import KalisioMaps from './components/KalisioMaps'
-import TemplateUrl from './components/TemplateUrl'
-import TourLink from './components/TourLink'
+import HomeFooter from './components/HomeFooter.vue'
+import KalisioMaps from './components/KalisioMaps.vue'
+import TemplateUrl from './components/TemplateUrl.vue'
+import TourLink from './components/TourLink.vue'
 import './custom.css'
 
 export default {
@@ -15,7 +15,7 @@ export default {
     // Use Quasar framework
     app.use(Quasar, { plugins: { Dialog } }, { req: { headers: {} } })
     // Dynamic registration of all components
-    app.component('KHomeFooter', KHomeFooter)
+    app.component('HomeFooter', HomeFooter)
     app.component('KalisioMaps', KalisioMaps)
     app.component('TemplateUrl', TemplateUrl)
     app.component('TourLink', TourLink)
