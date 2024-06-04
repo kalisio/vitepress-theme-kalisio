1cfee0144eaefcb3c61852f265e48c73eb74c617
cnouguier <ch.nouguier@gmail.com>
2024-06-04 14:03:21 +0200

    wip: Provide a docs sample #8

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
-  base: '/',
+  base: '/vitepress-theme-kalisio/',
   title: " ",
   description: "Kalisio Vitepress Theme",
   lastUpdated: true,
