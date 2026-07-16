011850c5739add9d1e08903ff2ea9817e5e207b8
Devneb01 <chahbouniali37@gmail.com>
2026-07-16 14:58:15 +0000

    Docs built from 079ad558b4a365c2aa4cec816f08e2d3aaa6294d

---
+++ assets/about_license.md.DXS6cNdX.js
@@ -0,0 +1,21 @@
+import{_ as n,o as a,c as e,ah as p}from"./chunks/framework.DOIUgTXc.js";const I=JSON.parse('{"title":"License","description":"","frontmatter":{},"headers":[],"relativePath":"about/license.md","filePath":"about/license.md","lastUpdated":1784213847000}'),i={name:"about/license.md"};function t(l,s,o,c,r,T){return a(),e("div",null,[...s[0]||(s[0]=[p(`<h1 id="license" tabindex="-1">License <a class="header-anchor" href="#license" aria-label="Permalink to &quot;License&quot;">​</a></h1><p><strong>vitepress-theme-kalisio</strong> is, and always will be, 100% Open Source under the MIT license.</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>MIT License</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Copyright (c) 2023-20xx Kalisio</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Permission is hereby granted, free of charge, to any person obtaining a copy</span></span>
+<span class="line"><span>of this software and associated documentation files (the &quot;Software&quot;), to deal</span></span>
+<span class="line"><span>in the Software without restriction, including without limitation the rights</span></span>
+<span class="line"><span>to use, copy, modify, merge, publish, distribute, sublicense, and/or sell</span></span>
+<span class="line"><span>copies of the Software, and to permit persons to whom the Software is</span></span>
+<span class="line"><span>furnished to do so, subject to the following conditions:</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>The above copyright notice and this permission notice shall be included in all</span></span>
+<span class="line"><span>copies or substantial portions of the Software.</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>THE SOFTWARE IS PROVIDED &quot;AS IS&quot;, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR</span></span>
+<span class="line"><span>IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,</span></span>
+<span class="line"><span>FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE</span></span>
+<span class="line"><span>AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER</span></span>
+<span class="line"><span>LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,</span></span>
+<span class="line"><span>OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE</span></span>
+<span class="line"><span>SOFTWARE.</span></span></code></pre></div>`,3)])])}const h=n(i,[["render",t]]);export{I as __pageData,h as default};
